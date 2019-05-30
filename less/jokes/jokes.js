2b16c95effd1737e0c5980fa1b0d785765c5498c
Daryl Magera <darylmagera@gmail.com>
2019-05-30 13:22:47 -0400

    home page fully functional

--- less/jokes/jokes.js
+++ less/jokes/jokes.js
@@ -4,27 +4,35 @@ class Joke{
         // asigning this.attribute to the passed in attribute
         this.attribute = attribute;
 
-        // creating a reference to the '.view-joke' class
-        this.viewJoke = attribute.querySelector('.view-joke')
+        // creating a reference to the 'button' tag
+        this.viewJoke = attribute.querySelector('button')
+        
+
         //console.log(`my button aye: ${this.viewJoke}`);
 
-        // updating the text of my view-joke button
-        this.viewJoke.textContent = 'View More Jokes'
+        // updating the text of my button
+        this.viewJoke.textContent = 'View Joke'
+        
 
-        // added a click handler on viewJoke reference that's going to trigger the viewContent method 
-        this.viewJoke.addEventListener('click', () =>{
-            this.viewContent()
-        });
+        // added a click handler on viewJoke reference that's going to trigger the displayButton method 
+        this.viewJoke.addEventListener('click', () => {
+            this.displayButton();
+        })
+        
     }
-    viewContent(){
-        
-        
+    displayButton(){
+    if(this.viewJoke.textContent === "View Joke"){
+      this.viewJoke.textContent = 'Close'
+    }
+    else{
+      this.viewJoke.textContent = 'View Joke'
+    }   
     }
 }
 
 //  Selected all classes named ".jokes" and assiged them to variable jokes. 
 let jokes = document.querySelectorAll('.joke')
-console.log(jokes);
+//console.log(jokes);
 jokes.forEach( (joke) => {
 return new Joke(joke);
 }); 
---
+++ less/jokes/jquery/jquery.js
@@ -0,0 +1,32 @@
+// researched and used jquery
+// my code starts here
+$(document).ready(function(){
+    
+    $('.view-joke1').click(function(){
+        $('.joke-open1').toggle(1000, function(){
+         // $('.view-joke1').fadeIn();
+         
+          
+          
+        });
+        
+    });
+    $('.view-joke2').click(function(){
+        $('.joke-open2').toggle(1000, function(){
+          //$('.view-joke2').fadeIn(1000);
+        });
+        
+    });
+    $('.view-joke3').click(function(){
+        $('.joke-open3').toggle(1000, function(){
+          //$('.view-joke3').fadeIn();
+        });
+        
+    });
+    $('.view-joke4').click(function(){
+        $('.joke-open4').toggle(1000, function(){
+          //$('.view-joke4').fadeIn();
+        });
+        
+    });
+});
